ec24c4d884f55eb3e104e2ac09e7b8f92fe53f61
javeria2108 <javeriazh@gmail.com>
2024-06-21 01:22:48 +0500

    .

--- Backend/TaskManagement.Api/Program.cs
+++ Backend/TaskManagement.Api/Program.cs
@@ -1,7 +1,9 @@
+using TaskManagement.Api.Data;
 using TaskManagement.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var connString=builder.Configuration.GetConnectionString("Users");
+builder.Services.AddSqlite<UserContext>(connString);
 var app = builder.Build();
 
 app.MapUsersEndpoints();
