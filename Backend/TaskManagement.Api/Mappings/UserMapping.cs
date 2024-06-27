ebb12aa008a3a4fab1c97afb22af88bdd917dc08
javeria2108 <javeriazh@gmail.com>
2024-06-27 18:15:14 +0500

    user switched to identity user

--- Backend/TaskManagement.Api/Mappings/UserMapping.cs
+++ Backend/TaskManagement.Api/Mappings/UserMapping.cs
@@ -1,38 +1,38 @@
-﻿using TaskManagement.Api.Dtos.UserDtos;
-using TaskManagement.Api.Models;
+﻿// using TaskManagement.Api.Dtos.UserDtos;
+// using TaskManagement.Api.Models;
 
-namespace TaskManagement.Api.Mappings;
+// namespace TaskManagement.Api.Mappings;
 
-public static class UserMapping
-{
-    public static User ToEntity(this CreateUserDto user){
-        return new User(){
-            Name=user.Name,
-            Email=user.Email,
-            Password=user.Password
-        };
-    }
-    public static UserDetailsDto ToDetailsDto(this User user){
-        return new (
-            user.Id,
-            user.Name,
-            user.Email
-        );
-    }
-     public static UserSummaryDto ToSummaryDto(this User user){
-        return new (
-            user.Id,
-            user.Name,
-            user.Email);
-    }
+// public static class UserMapping
+// {
+//     public static User ToEntity(this CreateUserDto user){
+//         return new User(){
+//             Name=user.Name,
+//             Email=user.Email,
+//             Password=user.Password
+//         };
+//     }
+//     public static UserDetailsDto ToDetailsDto(this User user){
+//         return new (
+//             user.Id,
+//             user.Name,
+//             user.Email
+//         );
+//     }
+//      public static UserSummaryDto ToSummaryDto(this User user){
+//         return new (
+//             user.Id,
+//             user.Name,
+//             user.Email);
+//     }
 
-    public static User ToEntity(this UpdateUserDto user,int id){
-        return new User(){
-            Id=id,
-            Name=user.Name,
-            Email=user.Email,
-            Password=user.Password
-        };
-    }
+//     public static User ToEntity(this UpdateUserDto user,string id){
+//         return new User(){
+//             Id=id,
+//             Name=user.Name,
+//             Email=user.Email,
+//             Password=user.Password
+//         };
+//     }
 
-}
+// }
