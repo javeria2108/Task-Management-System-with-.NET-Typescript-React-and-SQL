68926e40a6b9aa2b23df68a0a2bbdd596bab0558
javeria2108 <javeriazh@gmail.com>
2024-07-03 17:10:29 +0500

    migrating from redux legacy code to redux toolkit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,18 @@
 import './index.css';
 import {Register} from "./Users/Register"
-import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from './Users/Login';
-import { store } from './redux/store';
 
 function App() {
 
   return (
-    <Provider store={store}>
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Register/>}/>
       <Route path='/login' element={<Login/>}/>
     </Routes>
     </BrowserRouter>
-    </Provider>
+
     
   )
 }
