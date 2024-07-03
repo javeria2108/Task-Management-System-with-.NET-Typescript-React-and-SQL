3601a18fc55e20d2236d3cdf1ef81b451074f62a
javeria2108 <javeriazh@gmail.com>
2024-07-03 16:24:17 +0500

    redux state management for registration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import './index.css';
 import {Register} from "./Users/Register"
-
+import { Provider } from 'react-redux';
 function App() {
 
   return (
-    <>
+    <Provider store={}>
       <Register/>
-    </>
+      </Provider>
+    
   )
 }
 
