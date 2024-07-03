455cc0bad0de043b67e0bceac6e40f931ff4f48c
javeria2108 <javeriazh@gmail.com>
2024-07-03 16:43:25 +0500

    store added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,21 @@
 import './index.css';
 import {Register} from "./Users/Register"
-<<<<<<< HEAD
 import { Provider } from 'react-redux';
-function App() {
-
-  return (
-    <Provider store={}>
-      <Register/>
-      </Provider>
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from './Users/Login';
+import { store } from './redux/store';
 
 function App() {
 
   return (
+    <Provider store={store}>
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Register/>}/>
       <Route path='/login' element={<Login/>}/>
     </Routes>
     </BrowserRouter>
-
->>>>>>> e6df2cf2bc4591fc40b2f24a6846a871beee0a76
+    </Provider>
     
   )
 }
